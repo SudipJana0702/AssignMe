a9d124422844ab4d54ece74c7db67552249199c2
Sudip Jana <22ucc105@lnmiit.ac.in>
2024-07-24 14:39:27 +0530

    Add files via upload

---
+++ src/main/java/com/example/assigntodo/auth/SigninActivity.kt
@@ -0,0 +1,17 @@
+package com.example.assigntodo.auth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.assigntodo.R
+
+class SigninActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_signin)
+
+    }
+}
